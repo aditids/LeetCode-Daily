9db27c352960ac301c07dffec3ca5df5925102f1
Sonawane, Aditi Dilip <asonawan@iu.edu>
2024-01-28 19:36:58 -0500

    Jan 29

---
+++ 232. Implement Queue using Stacks.py
@@ -0,0 +1,28 @@
+# 232. Implement Queue using Stacks
+class MyQueue:
+
+    def __init__(self):
+        self.queue = []
+
+    def push(self, x: int) -> None:
+        self.queue.append(x)
+
+    def pop(self) -> int:
+        k = self.queue[0]
+        self.queue.remove(k)
+        return k
+               
+    def peek(self) -> int:
+        return self.queue[0]
+
+    def empty(self) -> bool:
+        return len(self.queue)==0
+        
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
