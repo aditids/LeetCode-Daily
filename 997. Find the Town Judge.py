0dde2e4092965c229b7ae7d76504bec52713c59c
Sonawane, Aditi Dilip <asonawan@iu.edu>
2024-02-22 16:43:46 -0500

    Feb 22

--- 997. Find the Town Judge.py
+++ 997. Find the Town Judge.py
@@ -2,18 +2,31 @@
 
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
-        if trust == [] and n==1: return 1
-        graph = {}
-        k = -1
-        for pair in trust:
-            if pair[1] not in graph:
-                graph[pair[1]] = []
-            graph[pair[1]].append(pair[0])
-        for i in graph:
-            if len(graph[i]) == n-1:
-                k = i
+        total = defaultdict(int)
+        for i,j in trust:
+            total[i]-=1
+            total[j]+=1
+        for i in range(1,n+1):
+            if total[i]==n-1:
+                return i
+        return -1
+    
+    # Time complexity - O(v+e)
+
+
+
+        # if trust == [] and n==1: return 1
+        # graph = {}
+        # k = -1
+        # for pair in trust:
+        #     if pair[1] not in graph:
+        #         graph[pair[1]] = []
+        #     graph[pair[1]].append(pair[0])
+        # for i in graph:
+        #     if len(graph[i]) == n-1:
+        #         k = i
         
-        for i in graph:
-            if k in graph[i]:
-                return -1
-        return k
+        # for i in graph:
+        #     if k in graph[i]:
+        #         return -1
+        # return k
