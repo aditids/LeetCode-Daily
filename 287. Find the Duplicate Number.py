22e983438790e47a1c967d6b87616bdd5dde9526
Sonawane, Aditi Dilip <asonawan@iu.edu>
2024-03-23 20:19:12 -0400

    March 24

---
+++ 287. Find the Duplicate Number.py
@@ -0,0 +1,19 @@
+# 287. Find the Duplicate Number
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        slow = fast = nums[0]
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow==fast:
+                break
+
+        slow = nums[0]
+        while slow!=fast:
+            slow = nums[slow]
+            fast = nums[fast]
+        return slow
+
+        # Time complexity = O(n)
+        # Space complexity = O(1)
